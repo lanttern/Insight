d0b6d8682f92449774079e5039c0dbcd86cf7aa1
Zhihui Xie <xiewisdom@gmail.com>
2017-04-13 17:23:34 -0700

    update codes

--- sws/BackendCode/sampling_data.py
+++ sws/BackendCode/sampling_data.py
@@ -2,19 +2,54 @@
 # -*- coding: utf-8 -*-
 """
 @author: zhihuixie
-this program is used to sample out discharge summary for note events table
 """
 import pandas as pd
-# set up path to read file and path to output file
-in_file_name = '../DataSet/NOTEEVENTS.csv'
-out_file_name = '../DataSet/note_events_discharge.csv'
-# set up column names to get data
-cols = ['SUBJECT_ID', 'HADM_ID', 'CATEGORY', 'ISERROR', 'TEXT']
-# iterate and process 500000 rows from data each time and retrieve discharge summary category
-iter_csv = pd.read_csv(in_file_name, iterator=True, chunksize = 500000, usecols = cols)
-print 'Compelte loading data and starting to processing!'
-df = pd.concat([chunk[chunk['CATEGORY'] == 'Discharge summary'] for chunk in iter_csv])
-print 'Complete processing!'
-# write data in pandas dataframe to a csv file
-df.to_csv(out_file_name, index = False)
-print 'Data are written successfully!'
+
+class Sampling():
+    """
+    this program is used to sample out discharge summary for note events table
+    """
+    def __init__(self, in_file_name, out_file_name):
+        """
+        initiate two parameters:
+            @in_file_name: file name of input data
+            @out_file_name: file name of output data
+        """
+        self.in_file_name = in_file_name
+        self.out_file_name = out_file_name
+        
+    def read_file(self, cols, chunksize):
+        """
+        read a file into pandas dataframe with the chuncksize
+        @cols: cols used to extract
+        @chunksize: size of chunks to read into the dataframe
+        """
+        self.iter_csv = pd.read_csv(in_file_name, iterator=True, 
+                                    chunksize = chunksize, usecols = cols)
+        print 'Compelte loading data and starting to processing!'
+        
+    def save_file(self, col_value, col_name, cols, chunksize):
+        """
+        save to file to local
+        @col_value: filter to get the target rows
+        @col_name: filter to target to the specific column
+        @cols: cols used to extract
+        @chunksize: size of chunks to read into the dataframe
+        """
+        self.read_file(cols, chunksize)
+        df = pd.concat([chunk[chunk[col_name] == col_value] for chunk in self.iter_csv])
+        print 'Complete processing!'
+        df.to_csv(self.out_file_name, index = False)
+        print 'Data are written successfully!'
+
+if __name__ == "__main__":
+    # set up path to read file and path to output file
+    in_file_name = '../DataSet/NOTEEVENTS.csv'
+    out_file_name = '../DataSet/note_events_discharge.csv'
+    # set up column names to get data
+    cols = ['SUBJECT_ID', 'HADM_ID', 'CATEGORY', 'ISERROR', 'TEXT']
+    chunksize = 500000
+    col_name = 'CATEGORY'
+    col_value = 'Discharge summary'
+    sample = Sampling(in_file_name, out_file_name)
+    sample.save_file(col_value, col_name, cols, chunksize)
--- sws/BackendCode/wiki_med_dict.py
+++ sws/BackendCode/wiki_med_dict.py
@@ -12,41 +12,53 @@ import urllib2
 import re
 from sklearn.externals import joblib
 
-def med_dict_generator(titles):
-    '''
-    create a dictionary with key as a medical abbreviation and value as a 
+class MedDictGenerator():
+    """
+    this class creates a dictionary with key as a medical abbreviation and value as a 
     full term of the abbreviation using resource from wiki page
-    titles - a list of wiki searching keywords
-    '''
-    med_dict = {}
-    for title in titles:
-        # get wiki link
-        med_abbr = wiki.page(title)
-        link = med_abbr.url
-        # request content from wiki page
-        req = urllib2.Request(link)
-        page = urllib2.urlopen(req)
-        soup = BeautifulSoup(page, "lxml")
-        # processing the content
-        table = soup.find('table', { 'class' : 'wikitable sortable' })
-        for row in table.findAll('tr'):
-            cells = row.findAll('td')
-            try:
-                # get the medical abbreviation
-                key = cells[0].find(text = True).encode('utf-8')
-                key = re.sub(' +', ' ', key)
-                # get the corresponding full medical terms and clean the values
-                values = cells[1].findAll(text = True)
-                value = ' '.join([v.lower() for v in values])
-                value = value.split('\n')[0].strip().encode('utf-8')
-                value = re.sub('\(.*?\)', ' ', value)
-                value = re.sub('[^0-9a-zA-Z]', ' ', value)
-                value = re.sub('[ ]+', ' ', value)
-                if (len(key) > 1 and key != 'on' )and key not in med_dict:
-                    med_dict[key] = value
-            except IndexError:
-                continue
-    return med_dict
+    """
+    def __init__(self, titles):
+        """
+        @titles - a list of wiki searching keywords
+        """
+        self.titles = titles
+
+    def med_dict_generator(self):
+        """
+        create a dictionary with key as a medical abbreviation and value as a 
+        full term of the abbreviation using resource from wiki page
+        return a dictionary with key as abbreviation of medical terms and value as 
+        full term of the abbreviation
+        """
+        med_dict = {}
+        for title in self.titles:
+            # get wiki link
+            med_abbr = wiki.page(title)
+            link = med_abbr.url
+            # request content from wiki page
+            req = urllib2.Request(link)
+            page = urllib2.urlopen(req)
+            soup = BeautifulSoup(page, "lxml")
+            # processing the content
+            table = soup.find('table', { 'class' : 'wikitable sortable' })
+            for row in table.findAll('tr'):
+                cells = row.findAll('td')
+                try:
+                    # get the medical abbreviation
+                    key = cells[0].find(text = True).encode('utf-8')
+                    key = re.sub(' +', ' ', key)
+                    # get the corresponding full medical terms and clean the values
+                    values = cells[1].findAll(text = True)
+                    value = ' '.join([v.lower() for v in values])
+                    value = value.split('\n')[0].strip().encode('utf-8')
+                    value = re.sub('\(.*?\)', ' ', value)
+                    value = re.sub('[^0-9a-zA-Z]', ' ', value)
+                    value = re.sub('[ ]+', ' ', value)
+                    if (len(key) > 1 and key != 'on' )and key not in med_dict:
+                        med_dict[key] = value
+                except IndexError:
+                    continue
+        return med_dict
 
 if __name__ == '__main__':
     # generate wiki search terms
@@ -55,11 +67,16 @@ if __name__ == '__main__':
     titles = [stem_title + '_' + tails[i] for i in range(len(tails))]
     titles.append(stem_title + '_0-9')
     # generate medical abbreviation-full term dictionary
-    med_dict = med_dict_generator(titles)
-    print len(med_dict)
+    generator = MedDictGenerator(titles)
+    med_dict = generator.med_dict_generator()
+     
     # test the med_dict
-    test_cases = ['3TC', 'HLD', 'HTN', 'Hx',  'PCa']
+    test_cases = ['3TC', 'HLD', 'HTN', 'PCa']
+    test_res = []
+    res = ['lamivudine', 'Hyperlipidemia', 'hypertension', 'prostate cancer']
     for key in test_cases:
-        print med_dict[key]
+        val = med_dict[key]
+        test_res.append(val)
+    assert(test_res == res)
     # save data
     joblib.dump(med_dict, '../DataSet/med_dict.plk')
